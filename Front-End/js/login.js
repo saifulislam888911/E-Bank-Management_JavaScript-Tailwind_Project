8c4a17f1c63bfa3aa7447dafd7cd83f4c239b6ef
SAIFUL ISLAM <saiful.islam888911@gmail.com>
2024-04-29 04:40:11 +0600

    Dev-1 Commit No.1 - Deposit, Withdraw - commit 1
    # Html Page Added : index , user_Profile .
    # Js File Added : login , deposit , calculation_Mine .
    
    # UI/UX Design : user_Profile.html .
    
    # Functionality : 1. user_Profile.html :
                         i. calculation_Mine.js
    
    # Features :
                1. user_Profile.html :
                   i. Calculation : Deposit, Withdraw .

--- Front-End/js/login.js
+++ Front-End/js/login.js
@@ -2,23 +2,9 @@ console.log('JS File : login.js');
 
 
 
-/* Note :
-   | Step 1 : add click event handler with the Login Button. 
-*/
 document.getElementById('btn-login').addEventListener('click', function(){
+    //console.log('Login Button Clicked');
 
-    console.log('Login Button Clicked');
-
-    /* Note :
-       | Step 2 : get email
-       | 2.a : set id on the html element. 
-       | 2.b : get the html element (email address from the inside of the input field). 
-       | 2.c : get the value/text from the html element 
-               - For input field, 
-               use ".value" to get text/value from an input field.
-               - For non-input element(element other than input, textarea),
-               use ".innerText" to get text/value from an element.
-    */
     const emailField = document.getElementById('user-email');
     const email = emailField.value;
 
